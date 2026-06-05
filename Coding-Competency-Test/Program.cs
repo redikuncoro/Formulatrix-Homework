a6910112d14b96a3d6aed05b8e9db0f8d6eba38b
redikuncoro <redikuncorokatri@gmail.com>
2026-06-05 22:59:42 +0700

    Coding Competency Test : 1. foobar

---
+++ Coding-Competency-Test/Program.cs
@@ -0,0 +1,35 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Input N :");
+        string input = Console.ReadLine();
+        int number;
+        if (!int.TryParse(input, out number))
+        {
+            Console.WriteLine("Invalid input. Please enter a valid integer.");
+            return;
+        }
+        
+        List<string> results = new List<string>();
+
+        for (int x = 1; x <= number; x++)
+        {
+            string result = "";
+
+            if (x % 3 == 0) result += "foo";
+            if (x % 5 == 0) result += "bar";
+
+            if (string.IsNullOrEmpty(result))
+            {
+                results.Add(x.ToString());
+            }
+            else
+            {
+                results.Add(result);
+            }
+        }
+
+        Console.WriteLine(string.Join(", ", results));
+    }
+}
